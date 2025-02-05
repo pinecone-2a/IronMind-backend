58773cc6b60c8ef189153ffd014e038f4e17bcd4
Zenitsu58494 <Zenitsu55555555@gmail.com>
2025-02-06 02:56:24 +0800

    first

--- router/donation.ts
+++ router/donation.ts
@@ -4,18 +4,26 @@ const express = require("express");
 export const donationRouter = Router();
 
 donationRouter.post("/create", async (req: Request, res: Response) => {
-  const user = await prisma.donation.create({
-    data: {
-      amount: 1000,
-      specialMessage: "Hello thank you so much for donating me",
-      socialURLOrBuyMeACoffe: "url link comes in ",
-      donorId: 1213131,
-      recipientId: 131313,
-    },
-  });
-  res.json(user);
+  try{
+    const user = await prisma.donation.create({
+      data: {
+        amount: req.body.amount,
+        specialMessage: req.body.specialMessage,
+        socialURLOrBuyMeACoffe: req.body.socialURLOrBuyMeACoffe,
+        donorId: 1213131,
+        recipientId: 131313,
+      },
+    });
+    res.json(user);
+  } catch{
+    res.send("failed to fetch")
+  }
 });
 donationRouter.get("/get", async (req: Request, res: Response) => {
+ try {
   const data = await prisma.donation.findMany();
   res.json(data);
+ } catch{
+  res.send("failed to fetch")
+ }
 });
