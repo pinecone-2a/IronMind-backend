ac12a56bd3d648634befbce91e3640fb5b70d2ad
azaaa-9 <az95342030@gmail.com>
2025-02-07 18:04:40 +0800

    seemore + share page +  scroll

--- router/donation.ts
+++ router/donation.ts
@@ -4,24 +4,56 @@ import { prisma } from "..";
 const express = require("express");
 export const donationRouter = Router();
 
-donationRouter.post("/", async (req: Request, res: Response) => {
+donationRouter.post("/create-donation/:recipientId", async (req: Request, res: Response) => {
+  const {recipientId} = req.params
+  const {body} = req
   const donation = await prisma.donation.create({
     data: {
-      amount: 8210,
-      specialMessage: "buy coffee",
-      socialURLOrBuyMeACoffee: "dhiwahdidaw",
+      ...body,
       donorId: "KEUUmof9rIwwDRYAGGzni",
-      recipientId: "nHvliOYxZesv2rMZ2lv1cdwdaw",
+      recipientId: recipientId,
     },
   });
   res.json(donation);
 });
-donationRouter.get("/", async (req: Request, res: Response) => {
-  const data = await prisma.donation.findMany({
-    include: {
-      donor: true,
-      recipient: true,
-    },
-  });
-  res.json(data);
-});
+
+
+// donationRouter.get("/", async (req: Request, res: Response) => {
+//   const data = await prisma.donation.findMany({
+//     include: {
+//       donor: true,
+//       recipient: true,
+//     },
+//   });
+//   res.json(data);
+// });
+
+donationRouter.get("/total-earnings/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId
+  const today = new Date()
+  const before30Day = today.getDay() - 30
+  const donations = await prisma.donation.findMany({
+    where: {
+      recipientId : userId
+    }
+  })
+
+  const totalEarnings = donations.reduce((acc, donation) => {
+    return acc + donation.amount;
+  },0)
+  res.json({totalEarnings : totalEarnings})
+})
+
+donationRouter.get("/recieved/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId
+  // const {} = req.body
+  const donations = await prisma.donation.findMany({
+    where: {
+      recipientId : userId,
+      amount:{
+        equals : 10
+      }
+    }
+  })
+  res.json(donations)
+})
