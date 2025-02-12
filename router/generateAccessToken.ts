08dd843087484687c964d56f324372669e01903f
24LP7884 24LP7884 <24LP7884@iMac-302-1F.local>
2025-02-12 19:29:50 +0800

    update

---
+++ router/generateAccessToken.ts
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken"
+
+export const generateAccessToken = (email: string) => {
+  const token = jwt.sign({email}, process.env.ACCESS_TOKEN_SECRET!, 
+    { expiresIn: "1m" });
+
+    return token;
+}
