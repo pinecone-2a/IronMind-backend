8f431d8971eb577fa004a6068e9becaad3f51f31
24LP3531 24LP3531 <24LP3531@setups-iMac-3.local>
2025-02-17 17:00:04 +0800

    cors origin fixed

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: ["http://localhost:3000", "https://iron-mind-frontend.vercel.app"],
     credentials: true,
   })
 );
